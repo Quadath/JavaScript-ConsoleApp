f70022140d68c38d9b89a57e890f919bfd084201
Quadath <91686101+Quadatz@users.noreply.github.com>
2021-12-01 18:18:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//const result = prompt("Вам есть 18?", "Да");
+// const result = +prompt("Вам есть 18?", "Да");
+// console.log(typeof(result));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+/*const user = "Ivan"
+
+alert(`Привет, ${user}`); */
+
+
